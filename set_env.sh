927de0671f34905400811b5f01f0819884f1e90f
Kafilat <Kafilat.sokunbi@gmail.com>
2022-09-28 10:24:01 +0100

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=alx_postgres
+export POSTGRES_PASSWORD=io0E5912irQongB
+export POSTGRES_HOST=project03db.crzopvoz9ukm.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::alxproject-03-s3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
