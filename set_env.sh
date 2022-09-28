6d57981f83213b2d259e597ce6c3c461230ea945
Kafilat <Kafilat.sokunbi@gmail.com>
2022-09-28 10:56:40 +0100

    updated my env file

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=alx_postgres
-export POSTGRES_PASSWORD=io0E5912irQongB
+export POSTGRES_PASSWORD=QJxpirkhgdyeUJTC
 export POSTGRES_HOST=project03db.crzopvoz9ukm.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::alxproject-03-s3
